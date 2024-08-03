7575fee87c79d21b1fe0fe8e41dc382211db97a6
nurlan60 <nurlan.dairbekov@gmail.com>
2024-08-03 14:17:23 +0500

    status

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,26 +1,11 @@
--- Show username and hostname in header
----------------------------------------
-function Header:host()
+Header:children_add(function()
 	if ya.target_family() ~= "unix" then
 		return ui.Line({})
 	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("magenta")
-end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
 
-function Header:render(area)
-	self.area = area
-
-	local right = ui.Line({ self:count(), self:tabs() })
-	local left = ui.Line({ self:host(), self:cwd(math.max(0, area.w - right:width())) })
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-	}
-end
-
--- Show user/group of files in status bar
-----------------------------------------
-function Status:owner()
+Status:children_add(function()
 	local h = cx.active.current.hovered
 	if h == nil or ya.target_family() ~= "unix" then
 		return ui.Line({})
@@ -32,39 +17,16 @@ function Status:owner()
 		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
 		ui.Span(" "),
 	})
-end
+end, 500, Status.RIGHT)
 
-function Status:render(area)
-	self.area = area
-
-	local left = ui.Line({ self:mode(), self:size(), self:name() })
-	-- local right = ui.Line({ self:owner(), self:permissions(), self:percentage(), self:position() })
-	local right = ui.Line({ self:owner(), self:permissions() })
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-		table.unpack(Progress:render(area, right:width())),
-	}
-end
-
--- Show simlynk in status bar
-----------------------------
 function Status:name()
-	local h = cx.active.current.hovered
+	local h = self._tab.current.hovered
 	if not h then
-		return ui.Span("")
+		return ui.Line({})
 	end
-
 	local linked = ""
 	if h.link_to ~= nil then
 		linked = " -> " .. tostring(h.link_to)
-	else
-		linked = h.name
 	end
-	return ui.Span(" " .. linked)
+	return ui.Line(" " .. h.name .. linked)
 end
-
--- zoxide update db
-require("zoxide"):setup({
-	update_db = true,
-})
