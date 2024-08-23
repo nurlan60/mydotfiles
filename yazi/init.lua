9638cfc69864fb77080e29bf5ca564112543e0a6
nurlan60 <nurlan.dairbekov@gmail.com>
2024-08-24 03:45:50 +0500

    yazi host

--- yazi/init.lua
+++ yazi/init.lua
@@ -9,3 +9,10 @@ function Status:name()
 	end
 	return ui.Line(" " .. h.name .. linked)
 end
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
