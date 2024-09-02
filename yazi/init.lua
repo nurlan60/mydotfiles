f77b51073ebf6bcb0c6f7ceb733be13758a33cb8
nurlan60 <nurlan.dairbekov@gmail.com>
2024-09-02 18:32:34 +0500

    bookmarks persist

--- yazi/init.lua
+++ yazi/init.lua
@@ -16,3 +16,10 @@ Header:children_add(function()
 	end
 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end, 500, Header.LEFT)
+
+-- Bookmarks setup
+require("bookmarks"):setup({
+	persist = "vim",
+	desc_format = "parent",
+	file_pick_mode = "parent",
+})
