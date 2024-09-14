bdf0b56fa2c6d373874b673bd1ef7e626fcab974
nurlan60 <nurlan.dairbekov@gmail.com>
2024-09-14 06:23:46 +0500

    update

--- yazi/init.lua
+++ yazi/init.lua
@@ -4,17 +4,17 @@ require("session"):setup({
 })
 
 -- show symlinks in status
-function Status:name()
-	local h = self._tab.current.hovered
-	if not h then
-		return ui.Line({})
-	end
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-	return ui.Line(" " .. h.name .. linked)
-end
+-- function Status:name()
+-- 	local h = self._tab.current.hovered
+-- 	if not h then
+-- 		return ui.Line({})
+-- 	end
+-- 	local linked = ""
+-- 	if h.link_to ~= nil then
+-- 		linked = " -> " .. tostring(h.link_to)
+-- 	end
+-- 	return ui.Line(" " .. h.name .. linked)
+-- end
 
 -- show host in header
 Header:children_add(function()
