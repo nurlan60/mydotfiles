1f15ce38412f1d91b338cc31162866abf59b88f6
nurlan60 <nurlan.dairbekov@gmail.com>
2024-09-16 03:37:59 +0500

    yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -16,13 +16,13 @@ require("session"):setup({
 -- 	return ui.Line(" " .. h.name .. linked)
 -- end
 
--- show host in header
-Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ui.Line({})
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
-end, 500, Header.LEFT)
+-- -- show host in header
+-- Header:children_add(function()
+-- 	if ya.target_family() ~= "unix" then
+-- 		return ui.Line({})
+-- 	end
+-- 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+-- end, 500, Header.LEFT)
 
 -- Bookmarks setup
 require("bookmarks"):setup({
