5e7eac460a621ba5fb0d5ade9975d8cde2bdfd44
nurlan60 <nurlan.dairbekov@gmail.com>
2024-12-14 05:17:51 +0500

    yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -2,3 +2,18 @@
 require("session"):setup({
 	sync_yanked = true,
 })
+
+require("bookmarks"):setup({
+	persist = "vim",
+	desc_format = "parent",
+	file_pick_mode = "parent",
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
