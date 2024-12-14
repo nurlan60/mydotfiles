4649159f892f43f8cc721f78aa50b93fc572721b
nurlan60 <nurlan.dairbekov@gmail.com>
2024-12-14 22:46:31 +0500

    yazi

--- .local/bin/viewer.sh
+++ .local/bin/viewer.sh
@@ -8,34 +8,34 @@
 
 case ${1:e} in
   pdf)
-    pdftotext -layout -nopgbrk $1 - | less 
+    pdftotext -layout -nopgbrk "$1" - | less 
     ;;
   md)
-    glow --style dark -p $1
+    glow --style dark -p "$1"
     ;;
   djvu)
-    djvutxt $1 | less
+    djvutxt "$1" | less
     ;;
   docx)
-    docx2txt.pl $1 - | less
+    docx2txt "$1" - | less
     ;;
   xlsx)
-    in2csv $1 | csvlook | less
+    in2csv "$1" | csvlook | less
     ;;
   scv)
-    csvlook $1 | less
+    csvlook "$1" | less
     ;;
   json)
-    jq -C . $1 | less -R
+    jq -C . "$1" | less -R
     ;;
   transmission)
-    transmission-show $1
+    transmission-show "$1"
     ;;
   tar | tgz | tbz* | txz | zip | 7z | gz | xz | lzma | bz* |lz4 | sz | zst | rar )
-    ouch list $1 --tree | less
+    ouch list "$1" --tree | less
     ;;
   *)
-  bat $1
+  bat "$1"
     ;;
 esac
 
