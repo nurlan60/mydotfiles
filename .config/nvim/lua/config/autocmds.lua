aeb0c164379cd12fe92da0071d3105641ffc6cbb
nurlan60 <nurlan.dairbekov@gmail.com>
2024-12-17 09:34:16 +0500

    nvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Turn off paste mode when leaving insert
+----------------
+-- autocmd("InsertLeave", {
+--   pattern = "*",
+--   command = "set nopaste",
+-- })
+
+------------------------------------------------------
+-- Disable the concealing in some file formats
+-------------------
+autocmd("FileType", {
+  pattern = { "tex", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
