2573979abfd37e8f61ee9006ec4f9408480632f5
nurlan60 <nurlan.dairbekov@gmail.com>
2025-04-09 07:18:25 +0500

    yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -9,17 +9,9 @@ require("session"):setup({
 	sync_yanked = true,
 })
 
-require("bookmarks"):setup({
-	persist = "vim",
-	desc_format = "parent",
-	file_pick_mode = "parent",
-	notify = {
-		enable = true,
-		timeout = 1,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
+require("bunny"):setup({
+	hops = {
+		{ key = "m", path = "~/onedrive-sdu/teach/Matan4", desc = "Mat An" },
+		{ key = "f", path = "~/onedrive-sdu/teach/FAN", desc = "Func An" },
 	},
 })
