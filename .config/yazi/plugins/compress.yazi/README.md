46f94a48d9f42e728ae3b5c3f235f78696207fec
nurlan60 <nurlan.dairbekov@gmail.com>
2025-04-29 20:56:54 +0500

    yazi

--- .config/yazi/plugins/compress.yazi/README.md
+++ .config/yazi/plugins/compress.yazi/README.md
@@ -1,48 +1,59 @@
-# ~~archive.yazi~~ compress.yazi
+# compress.yazi
+
+<!--toc:start-->
+
+- [compress.yazi](#compressyazi)
+  - [Supported file types](#supported-file-types)
+  - [New features of this fork](#new-features-of-this-fork)
+  - [Install](#install)
+  - [Usage](#usage)
+  <!--toc:end-->
 
 A Yazi plugin that compresses selected files to an archive. Supporting yazi versions 0.2.5 and up.
 
 ## Supported file types
 
-| Extention     | Unix Command  | Windows Command |
-| ------------- | ------------- | --------------- |
-| .zip          | zip -r        | 7z a -tzip      |
-| .7z           | 7z a          | 7z a            |
-| .tar          | tar rpf       | tar rpf         |
-| .tar.gz       | gzip          | 7z a -tgzip     |
-| .tar.xz       | xz            | 7z a -txz       |
-| .tar.bz2      | bzip2         | 7z a -tbzip2    |
-| .tar.zst      | zstd          | zstd            |
-
+| Extention | Unix Command    | Windows Command |
+| --------- | --------------- | --------------- |
+| .zip      | 7z a -tzip      | 7z a -tzip      |
+| .7z       | 7z a            | 7z a            |
+| .rar      | rar (not unrar) | rar             |
+| .tar      | tar rpf         | tar rpf         |
+| .tar.gz   | gzip            | 7z a -tgzip     |
+| .tar.xz   | xz              | 7z a -txz       |
+| .tar.bz2  | bzip2           | 7z a -tbzip2    |
+| .tar.lz4  | lz4             | lz4             |
+| .tar.zst  | zstd            | zstd            |
 
 **NOTE:** Windows users are required to install 7-Zip and add 7z.exe to the `path` environment variable, only tar archives will be available otherwise.
 
+## New features of this fork
+
+- Support for `lha`, `lz4` and `rar` archives
+- Support input password to encrypt archives
 
 ## Install
 
 ```bash
-# For Unix platforms
-git clone https://github.com/KKV9/compress.yazi.git ~/.config/yazi/plugins/compress.yazi
-
-## For Windows
-git clone https://github.com/KKV9/compress.yazi.git %AppData%\yazi\config\plugins\compress.yazi
-
-# Or with yazi plugin manager
-ya pack -a KKV9/compress
+# with yazi plugin manager
+ya pack -a boydaihungst/compress
 ```
 
 - Add this to your `keymap.toml`:
+- Check default keymaps to prevent conflict https://github.com/sxyazi/yazi/blob/main/yazi-config/preset/keymap-default.toml
 
 ```toml
-[[manager.prepend_keymap]]
-on   = [ "c", "a" ]
-run  = "plugin compress"
-desc = "Archive selected files"
+[manager]
+  prepend_keymap = [
+    { on = [ "e", "c" ], run = "plugin compress", desc = "Compress file(s)" },
+    # If you input empty password, it will make an archive without password
+    { on = [ "e", "C" ], run = "plugin compress -- --secure", desc = "Compress file(s) with password" },
+  ]
 ```
 
 ## Usage
 
- - Select files or folders to add, then press `c` `a` to create a new archive.
- - Type a name for the new file. 
- - The file extention must match one of the supported filetype extentions.
- - The desired archive/compression command must be installed on your system.
+- Select files or folders or hover over a file, then press `e` `c` or `e` `C` to create a new archive.
+- Type a name for the new file and password (Optional).
+- The file extention must match one of the supported filetype extentions.
+- The desired archive/compression command must be installed on your system.
--- .config/yazi/plugins/compress.yazi/main.lua
+++ .config/yazi/plugins/compress.yazi/main.lua
@@ -1,40 +1,108 @@
 --- @since 25.2.7
 
+local supported_encryption = {
+	"%.zip$",
+	"%.7z$",
+	"%.rar$",
+	"%.lha$",
+}
+
 -- Send error notification
 local function notify_error(message, urgency)
 	ya.notify({
-		title = "Archive",
+		title = "Compress.yazi",
 		content = message,
 		level = urgency,
 		timeout = 5,
 	})
 end
 
+local function quote(path)
+	local result = "'" .. string.gsub(tostring(path), "'", "'\\''") .. "'"
+	return result
+end
+
+local function run_command(cmd, cwd)
+	if cwd then
+		-- escape path if it contains spaces
+		if package.config:sub(1, 1) == "\\" then
+			-- Windows
+			cmd = string.format('cd /d "%s" && %s', cwd, cmd)
+		else
+			-- Unix-like
+			cmd = string.format('cd "%s" && %s', cwd, cmd)
+		end
+	end
+
+	local handle = io.popen(cmd .. " 2>&1")
+	local output = handle:read("*a")
+	local success, exit_type, exit_code = handle:close()
+	return output, success, exit_type, exit_code
+end
+
 -- Check for windows
 local is_windows = ya.target_family() == "windows"
 
--- Make table of selected or hovered: path = filenames
-local selected_or_hovered = ya.sync(function()
-	local tab, paths, names, path_fnames = cx.active, {}, {}, {}
-	for _, u in pairs(tab.selected) do
-		paths[#paths + 1] = tostring(type(u.parent) == "function" and u:parent() or u.parent)
-		names[#names + 1] = tostring(type(u.name) == "function" and u:name() or u.name)
-	end
-	if #paths == 0 and tab.current.hovered then
-		paths[1] = tostring(
-			type(tab.current.hovered.url.parent) == "function" and tab.current.hovered.url:parent()
-				or tab.current.hovered.url.parent
-		)
-		names[1] = tostring(tab.current.hovered.name)
-	end
-	for idx, name in ipairs(names) do
-		if not path_fnames[paths[idx]] then
-			path_fnames[paths[idx]] = {}
+local function pathJoin(...)
+	-- Detect OS path separator ('\' for Windows, '/' for Unix)
+	local separator = package.config:sub(1, 1)
+	local parts = { ... }
+	local filteredParts = {}
+	-- Remove empty strings or nil values
+	for _, part in ipairs(parts) do
+		if part and part ~= "" then
+			table.insert(filteredParts, part)
 		end
-		table.insert(path_fnames[paths[idx]], name)
 	end
-	return path_fnames, tostring(tab.current.cwd)
+	-- Join the remaining parts with the separator
+	local path = table.concat(filteredParts, separator)
+	-- Normalize any double separators (e.g., "folder//file" → "folder/file")
+	path = path:gsub(separator .. "+", separator)
+
+	return path
+end
+
+local get_cwd = ya.sync(function()
+	return tostring(cx.active.current.cwd)
 end)
+
+local selected_files = ya.sync(function()
+	local tab, raw_urls = cx.active, {}
+	for _, u in pairs(tab.selected) do
+		raw_urls[#raw_urls + 1] = tostring(u)
+	end
+	return raw_urls
+end)
+
+local selected_or_hovered_files = ya.sync(function()
+	local tab, raw_urls = cx.active, selected_files()
+	if #raw_urls == 0 and tab.current.hovered then
+		raw_urls[1] = tostring(tab.current.hovered.url)
+	end
+	return raw_urls
+end)
+
+local selected_or_hovered = function()
+	local result = {}
+	local paths = selected_or_hovered_files()
+
+	for _, path in ipairs(paths) do
+		local url = Url(path)
+		local cha, err = fs.cha(url)
+
+		if cha then
+			local parent_path = tostring(type(url.parent) == "function" and url:parent() or url.parent)
+			if not result[parent_path] then
+				result[parent_path] = {}
+			end
+			table.insert(result[parent_path], quote(type(url.name) == "function" and url:name() or url.name))
+		else
+			notify_error(string.format("Failed to get metadata for %s: %s", path, err), "error")
+			return
+		end
+	end
+	return result
+end
 
 -- Check if archive command is available
 local function is_command_available(cmd)
@@ -47,109 +115,227 @@ local function is_command_available(cmd)
 	end
 
 	local cmd_exists = os.execute(stat_cmd)
-	if cmd_exists then
-		return true
-	else
-		return false
-	end
+	return cmd_exists
 end
 
 -- Archive command list --> string
 local function find_binary(cmd_list)
-	for _, cmd in ipairs(cmd_list) do
+	local idx = 1
+	local first_cmd, first_args
+	for cmd, args in pairs(cmd_list) do
 		if is_command_available(cmd) then
-			return cmd
+			return cmd, args
 		end
+		if idx == 1 then
+			first_cmd, first_args = cmd, args
+		end
+		idx = idx + 1
 	end
-	return cmd_list[1] -- Return first command as fallback
-end
-
--- Check if file exists
-local function file_exists(name)
-	local f = io.open(name, "r")
-	if f ~= nil then
-		io.close(f)
-		return true
-	else
-		return false
-	end
-end
-
--- Append filename to it's parent directory
-local function combine_url(path, file)
-	path, file = Url(path), Url(file)
-	return tostring(path:join(file))
+	return first_cmd, first_args -- Return first command as fallback
 end
 
 return {
-	entry = function()
+	entry = function(_, job)
 		-- Exit visual mode
 		ya.manager_emit("escape", { visual = true })
+		local secure = job.args.secure
+		local decrypt_password, input_pw_event
+		local files_to_archive = selected_or_hovered()
 
-		-- Define file table and output_dir (pwd)
-		local path_fnames, output_dir = selected_or_hovered()
-
-		-- Get input
-		local output_name, event = ya.input({
-			title = "Create archive:",
-			position = { "top-center", y = 3, w = 40 },
-		})
-		if event ~= 1 then
+		if files_to_archive == nil then
 			return
 		end
 
-		-- Use appropriate archive command
-		local archive_commands = {
-			["%.zip$"] = { command = "zip", args = { "-r" } },
-			["%.7z$"] = { command = { "7z", "7zz" }, args = { "a" } },
-			["%.tar.gz$"] = { command = "tar", args = { "rpf" }, compress = "gzip" },
-			["%.tar.xz$"] = { command = "tar", args = { "rpf" }, compress = "xz" },
-			["%.tar.bz2$"] = { command = "tar", args = { "rpf" }, compress = "bzip2" },
-			["%.tar.zst$"] = { command = "tar", args = { "rpf" }, compress = "zstd", compress_args = { "--rm" } },
-			["%.tar$"] = { command = "tar", args = { "rpf" } },
-		}
+		-- Get input
+		local output_name, event_name = ya.input({
+			title = "Create archive:",
+			position = { "top-center", y = 3, w = 40 },
+		})
 
-		if is_windows then
-			archive_commands = {
-				["%.zip$"] = { command = "7z", args = { "a", "-tzip" } },
-				["%.7z$"] = { command = "7z", args = { "a" } },
-				["%.tar.gz$"] = {
-					command = "tar",
-					args = { "rpf" },
-					compress = "7z",
-					compress_args = { "a", "-tgzip", "-sdel", output_name },
-				},
-				["%.tar.xz$"] = {
-					command = "tar",
-					args = { "rpf" },
-					compress = "7z",
-					compress_args = { "a", "-txz", "-sdel", output_name },
-				},
-				["%.tar.bz2$"] = {
-					command = "tar",
-					args = { "rpf" },
-					compress = "7z",
-					compress_args = { "a", "-tbzip2", "-sdel", output_name },
-				},
-				["%.tar.zst$"] = { command = "tar", args = { "rpf" }, compress = "zstd", compress_args = { "--rm" } },
-				["%.tar$"] = { command = "tar", args = { "rpf" } },
-			}
+		if event_name ~= 1 then
+			return
 		end
-
-		-- Match user input to archive command
-		local archive_cmd, archive_args, archive_compress, archive_compress_args
-		for pattern, cmd_pair in pairs(archive_commands) do
-			if output_name:match(pattern) then
-				archive_cmd = cmd_pair.command
-				archive_args = cmd_pair.args
-				archive_compress = cmd_pair.compress
-				archive_compress_args = cmd_pair.compress_args or {}
+		if secure then
+			decrypt_password, input_pw_event = ya.input({
+				title = "Enter password:",
+				obscure = true,
+				position = { "top-center", y = 3, w = 40 },
+			})
+			if input_pw_event ~= 1 then
+				return
+			end
+			if utf8.len(decrypt_password) > 1 then
+				local is_supported_encryption = false
+				for _, pattern in pairs(supported_encryption) do
+					if output_name:match(pattern) then
+						is_supported_encryption = true
+						break
+					end
+				end
+				if not is_supported_encryption then
+					notify_error("Unsupported encryption for file extention", "error")
+					return
+				end
 			end
 		end
 
-		-- Check if archive command has multiple names
-		if type(archive_cmd) == "table" then
-			archive_cmd = find_binary(archive_cmd)
+		local output_fpath = pathJoin(get_cwd(), output_name)
+		local output_furl = Url(output_fpath)
+		local output_fcha, _ = fs.cha(output_furl)
+		local output_fname_without_ext = type(output_furl.stem) == "function" and output_furl:stem() or output_furl.stem
+
+		-- Use appropriate archive command
+		local archive_commands = {
+			--  ───────────────────────────────── zip ─────────────────────────────────
+			["%.zip$"] = {
+				archive_commands = {
+					["7z"] = {
+						"a",
+						"-tzip",
+						decrypt_password and ("-p" .. quote(decrypt_password)) or "",
+						decrypt_password and "-mem=AES256" or "",
+					},
+					["7zz"] = {
+						"a",
+						"-tzip",
+						decrypt_password and ("-p" .. quote(decrypt_password)) or "",
+						decrypt_password and "-mem=AES256" or "",
+					},
+				},
+			},
+			--  ───────────────────────────────── 7z ──────────────────────────────
+			["%.7z$"] = {
+				archive_commands = {
+					["7z"] = {
+						"a",
+						"-t7z",
+						decrypt_password and ("-p" .. quote(decrypt_password)) or "",
+						decrypt_password and "-mem=AES256" or "",
+					},
+					["7zz"] = {
+						"a",
+						"-t7z",
+						decrypt_password and ("-p" .. quote(decrypt_password)) or "",
+						decrypt_password and "-mem=AES256" or "",
+					},
+				},
+			},
+			--  ───────────────────────────────── rar ─────────────────────────────────
+			["%.rar$"] = {
+				archive_commands = {
+					["rar"] = {
+						"a",
+						decrypt_password and ("-p" .. quote(decrypt_password)) or "",
+					},
+				},
+			},
+			--  ───────────────────────────────── lha ─────────────────────────────────
+			["%.lha$"] = {
+				archive_commands = {
+					["lha"] = {
+						"a",
+						decrypt_password and ("-p" .. quote(decrypt_password)) or "",
+					},
+				},
+			},
+			--  ──────────────────────────────── tar.gz ─────────────────────────────
+			["%.tar.gz$"] = {
+				archive_commands = {
+					["tar"] = {
+						"rpf",
+					},
+				},
+				compress_commands = {
+					["gzip"] = {},
+					["7z"] = {
+						"a",
+						"-tgzip",
+						"-sdel",
+						quote(output_name),
+					},
+				},
+			},
+			--  ───────────────────────────────── tar.xz ──────────────────────────────
+			["%.tar.xz$"] = {
+				archive_commands = {
+					["tar"] = {
+						"rpf",
+					},
+				},
+				compress_commands = {
+					["xz"] = {},
+					["7z"] = {
+						"a",
+						"-txz",
+						"-sdel",
+						quote(output_name),
+					},
+				},
+			},
+			--  ──────────────────────────────── tar.bz2 ────────────────────────────────
+			["%.tar.bz2$"] = {
+				archive_commands = {
+					["tar"] = {
+						"rpf",
+					},
+				},
+				compress_commands = {
+					["bzip2"] = {},
+					["7z"] = {
+						"a",
+						"-tbzip2",
+						"-sdel",
+						quote(output_name),
+					},
+				},
+			},
+			--  ──────────────────────────────── tar.zst ─────────────────────────────
+			["%.tar.zst$"] = {
+				archive_commands = {
+					["tar"] = {
+						"rpf",
+					},
+				},
+				compress_commands = {
+					["zstd"] = {
+						"--rm",
+					},
+					["7z"] = {
+						"a",
+						"-tzstd",
+						"-sdel",
+						quote(output_name),
+					},
+				},
+			},
+			--  ───────────────────────────────── tar.lz4 ─────────────────────────────────
+			["%.tar.lz4$"] = {
+				archive_commands = {
+					["tar"] = {
+						"rpf",
+					},
+				},
+				compress_commands = {
+					["lz4"] = {
+						"--rm",
+					},
+				},
+			},
+			--  ───────────────────────────────── tar ─────────────────────────────────
+			["%.tar$"] = {
+				archive_commands = {
+					["tar"] = { "rpf" },
+				},
+			},
+		}
+
+		-- Match user input to archive command
+		local archive_cmd, archive_args, compress_cmd, compress_args
+		for pattern, cmd_pair in pairs(archive_commands) do
+			if output_name:match(pattern) then
+				archive_cmd, archive_args = find_binary(cmd_pair.archive_commands)
+				compress_cmd, compress_args = find_binary(cmd_pair.compress_commands or {})
+			end
 		end
 
 		-- Check if no archive command is available for the extention
@@ -165,68 +351,81 @@ return {
 		end
 
 		-- Exit if compress command is not available
-		if archive_compress and not is_command_available(archive_compress) then
-			notify_error(string.format("%s compression not available", archive_compress), "error")
+		if compress_cmd and not is_command_available(compress_cmd) then
+			notify_error(string.format("%s compression not available", compress_cmd), "error")
 			return
 		end
 
-		-- If file exists show overwrite prompt
-		local output_url = combine_url(output_dir, output_name)
-		while true do
-			if file_exists(output_url) then
-				local overwrite_answer = ya.input({
-					title = "Overwrite " .. output_name .. "? y/N:",
-					position = { "top-center", y = 3, w = 40 },
+		local overwrite_answer = true
+		local output_fwithout_ext_cha = fs.cha(
+			Url(pathJoin(get_cwd(), type(output_furl.stem) == "function" and output_furl:stem() or output_furl.stem))
+		)
+		if output_fcha or output_fwithout_ext_cha then
+			local list_existed_files = {}
+			if output_fcha then
+				list_existed_files[#list_existed_files + 1] = ui.Span(output_name)
+			end
+			if output_fwithout_ext_cha then
+				list_existed_files[#list_existed_files + 1] = ui.Span(output_fwithout_ext_cha)
+			end
+			overwrite_answer = ya.confirm({
+				title = ui.Line("Create Archive File"),
+				content = ui.Text({
+					ui.Line(""),
+					ui.Line("The following file is existed, overwrite?"):fg("yellow"),
+					ui.Line(""),
+					ui.Line({
+						ui.Span(" "),
+						table.unpack(list_existed_files),
+					}):align(ui.Line.LEFT),
 				})
-				if overwrite_answer:lower() ~= "y" then
-					notify_error("Operation canceled", "warn")
-					return -- If no overwrite selected, exit
-				else
-					local rm_status, rm_err = os.remove(output_url)
-					if not rm_status then
-						notify_error(string.format("Failed to remove %s, exit code %s", output_name, rm_err), "error")
-						return
-					end -- If overwrite fails, exit
-				end
+					:align(ui.Text.LEFT)
+					:wrap(ui.Text.WRAP),
+				pos = { "center", w = 70, h = 10 },
+			})
+
+			if not overwrite_answer then
+				return -- If no overwrite selected, exit
 			end
-			if archive_compress and not output_name:match("%.tar$") then
-				output_name = output_name:match("(.*%.tar)") -- Test for .tar and .tar.*
-				output_url = combine_url(output_dir, output_name) -- Update output_url
-			else
-				break
+			local rm_status, rm_err = fs.remove("file", output_furl)
+			if not rm_status then
+				notify_error(string.format("Failed to remove %s, exit code %s", output_name, rm_err), "error")
+				return
 			end
 		end
 
-		-- Add to output archive in each path, their respective files
-		for path, names in pairs(path_fnames) do
-			local archive_status, archive_err =
-				Command(archive_cmd):args(archive_args):arg(output_url):args(names):cwd(path):spawn():wait()
-			if not archive_status or not archive_status.success then
+		for parent_path, fnames in pairs(files_to_archive) do
+			-- Add to output archive in each path, their respective files
+			local archive_output, archive_success, archive_code = run_command(
+				archive_cmd
+					.. " "
+					.. table.concat(archive_args, " ")
+					.. " "
+					.. quote(tostring(compress_cmd and output_fname_without_ext or output_fpath))
+					.. " "
+					.. table.concat(fnames, " "),
+				parent_path
+			)
+			if not archive_success then
 				notify_error(
-					string.format(
-						"%s with selected files failed, exit code %s",
-						archive_args,
-						archive_status and archive_status.code or archive_err
-					),
+					string.format("Failed to archive, error %s", archive_output and archive_output or archive_code),
 					"error"
 				)
+				return
 			end
 		end
 
-		-- Use compress command if needed
-		if archive_compress then
-			local compress_status, compress_err =
-				Command(archive_compress):args(archive_compress_args):arg(output_name):cwd(output_dir):spawn():wait()
-			if not compress_status or not compress_status.success then
+		if compress_cmd then
+			local compress_output, compress_success, compress_code = run_command(
+				compress_cmd .. " " .. table.concat(compress_args, " ") .. " " .. quote(output_fname_without_ext),
+				tostring(type(output_furl.parent) == "function" and output_furl:parent() or output_furl.parent)
+			)
+			if not compress_success then
 				notify_error(
-					string.format(
-						"%s with %s failed, exit code %s",
-						archive_compress,
-						output_name,
-						compress_status and compress_status.code or compress_err
-					),
+					string.format("Failed to compress, error %s", compress_output and compress_output or compress_code),
 					"error"
 				)
+				return
 			end
 		end
 	end,
