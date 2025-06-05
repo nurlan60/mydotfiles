867ffe9a4c3acc3428b1115a98abb16a021c9f58
nurlan60 <nurlan.dairbekov@gmail.com>
2025-06-05 19:49:34 +0500

    yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,15 @@
 require("eza-preview"):setup({
-	follow_symlinks = true,
+  -- Directory depth level for tree preview (default: 3)
+  level = 1,
+
+  -- Follow symlinks when previewing directories (default: false)
+  follow_symlinks = true,
+
+  -- Show target file info instead of symlink info (default: false)
+  dereference = false,
+
+  -- Show hidden files (default: true) 
+  all = false
 })
 
 -- cross-instance yank
