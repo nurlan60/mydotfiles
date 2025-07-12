e9ed17cc65d440f81739497042e6f6e8269163cd
nurlan60 <nurlan.dairbekov@gmail.com>
2025-07-12 05:50:39 +0500

    yazi

---
+++ .config/yazi-asus/init.lua
@@ -0,0 +1,21 @@
+-- cross-instance yank
+require("session"):setup({
+	sync_yanked = true,
+})
+
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false, mode = "dir" },
+	persist = "vim",
+	desc_format = "parent",
+	file_pick_mode = "parent",
+	custom_desc_input = false,
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
