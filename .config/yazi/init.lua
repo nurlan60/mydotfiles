9cac485e8c8d15d86bc3ab4a7b4430f739207e7e
nurlan60 <nurlan.dairbekov@gmail.com>
2025-08-17 22:29:09 +0500

    yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,18 +1,7 @@
-require("eza-preview"):setup({
-	-- Set the tree preview to be default (default: true)
-	default_tree = true,
-
-	-- Directory depth level for tree preview (default: 3)
-	level = 3,
-
-	-- Follow symlinks when previewing directories (default: false)
-	follow_symlinks = true,
-
-	-- Show target file info instead of symlink info (default: false)
-	dereference = false,
-
-	-- Show hidden files (default: true)
-	all = true,
+require("fuse-archive"):setup({
+	smart_enter = true,
+	mount_options = "nocache,nosymlinks",
+	mount_root_dir = os.getenv("HOME") .. "/tmp",
 })
 
 -- cross-instance yank
