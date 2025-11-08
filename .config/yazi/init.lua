6f73d4ce7f5c3afe52d80d2e516145fada0bfd1c
nurlan60 <nurlan.dairbekov@gmail.com>
2025-11-08 19:43:37 +0500

    yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -11,3 +11,32 @@ require("session"):setup({
 })
 
 require("whoosh"):setup({})
+
+require("mux"):setup({
+	aliases = {
+		eza_tree_1 = {
+			previewer = "piper",
+			args = {
+				'cd "$1" && LS_COLORS="ex=32" eza --oneline --tree --level 1 --color=always --icons=always --group-directories-first --no-quotes .',
+			},
+		},
+		eza_tree_2 = {
+			previewer = "piper",
+			args = {
+				'cd "$1" && LS_COLORS="ex=32" eza --oneline --tree --level 2 --color=always --icons=always --group-directories-first --no-quotes .',
+			},
+		},
+		eza_tree_3 = {
+			previewer = "piper",
+			args = {
+				'cd "$1" && LS_COLORS="ex=32" eza --oneline --tree --level 3 --color=always --icons=always --group-directories-first --no-quotes .',
+			},
+		},
+		eza_tree_4 = {
+			previewer = "piper",
+			args = {
+				'cd "$1" && LS_COLORS="ex=32" eza --oneline --tree --level 4 --color=always --icons=always --group-directories-first --no-quotes .',
+			},
+		},
+	},
+})
