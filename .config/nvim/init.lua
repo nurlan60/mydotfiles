1c29e1ddde66cd0696281531c6e627a5d85b7d3f
nurlan60 <nurlan.dairbekov@gmail.com>
2026-01-17 11:05:39 +0500

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,7 +42,7 @@ vim.cmd.colorscheme('tokyonight')
 -----------------------------------------------------------
 --- Latex ---
 MiniDeps.add('lervag/vimtex')
-vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_view_method = 'sioyek'
 vim.g.vimtex_quickfix_open_on_warning = 0
 vim.g.vimtex_quickfix_ignore_filters = {
          "Underfull \\hbox",
