1ea7b0ff1a8438e282996a96224e7625b7c3f7cd
nurlan60 <nurlan.dairbekov@gmail.com>
2026-01-18 17:32:17 +0500

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,8 @@ require('mini.deps').setup({})
 MiniDeps.add('folke/tokyonight.nvim')
 vim.cmd.colorscheme('tokyonight')
 ---------------------------------------------------
+require('mini.starter').setup()
+require('mini.bracketed').setup()
 require('mini.icons').setup()
 require('mini.statusline').setup()
 require('mini.tabline').setup() 
@@ -29,6 +31,7 @@ require('mini.indentscope').setup()
 require('mini.snippets').setup({})
 require('mini.completion').setup({})
 require('mini.pairs').setup({})
+require('mini.surround').setup()
 ---------------------------------------------------
 require('mini.files').setup({
 	mappings = {
@@ -41,6 +44,49 @@ require('mini.pick').setup({})
 vim.keymap.set('n', '<leader><space>', '<cmd>Pick buffers<cr>', {desc = 'Search open files'})
 vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<cr>', {desc = 'Search all files'})
 vim.keymap.set('n', '<leader>fh', '<cmd>Pick help<cr>', {desc = 'Search help tags'})
+---------------------------------------------------
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = { 'n', 'x' }, keys = '<Leader>' },
+
+    -- `[` and `]` keys
+    { mode = 'n', keys = '[' },
+    { mode = 'n', keys = ']' },
+
+    -- Built-in completion
+    { mode = 'i', keys = '<C-x>' },
+
+    -- `g` key
+    { mode = { 'n', 'x' }, keys = 'g' },
+
+    -- Marks
+    { mode = { 'n', 'x' }, keys = "'" },
+    { mode = { 'n', 'x' }, keys = '`' },
+
+    -- Registers
+    { mode = { 'n', 'x' }, keys = '"' },
+    { mode = { 'i', 'c' }, keys = '<C-r>' },
+
+    -- Window commands
+    { mode = 'n', keys = '<C-w>' },
+
+    -- `z` key
+    { mode = { 'n', 'x' }, keys = 'z' },
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.square_brackets(),
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+})
 ----------------------------------------------------
 --- VimTex ---
 MiniDeps.add('lervag/vimtex')
