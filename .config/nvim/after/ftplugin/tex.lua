815efd2618119cacff73dff7ffd9563913b082f1
nurlan60 <nurlan.dairbekov@gmail.com>
2026-03-21 10:55:12 +0500

    nvim

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,4 +1,16 @@
-local cmd = [[
-  :!latexmk -pdf -pv -e '$pdf_previewer="open -a Skim";' %<CR>
-]]
-vim.keymap.set('n', '<Localleader>ls',cmd, { buffer = true })
+local cmd
+local viewer
+
+local os_name = vim.loop.os_uname().sysname
+
+if os_name == "Linux" then
+  viewer = "zathura"
+elseif os_name == "Darwin" then
+  viewer = "open -a Skim"
+elseif os_name == "Windows_NT" then
+  viewer = ""
+end
+
+cmd = string.format(":!latexmk -pdf -pv -e '$pdf_previewer=\"%s\";' %%<CR>",viewer)
+
+vim.keymap.set('n', '<Localleader>ls', cmd, { buffer = true })
