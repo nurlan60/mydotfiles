281d964de9a80c6c8cbbe8e805a7231233f012aa
nurlan60 <nurlan.dairbekov@gmail.com>
2026-04-03 11:27:09 +0500

    nvim

--- .config/nvim/plugin/40_plugins.lua
+++ .config/nvim/plugin/40_plugins.lua
@@ -194,5 +194,22 @@ vim.g.vimtex_quickfix_ignore_filters = {
 	"LaTeX Warning: Unused global option(s):",
 }
 
-vim.cmd("packadd kbswitch.nvim")
-require("kbswitch").setup()
+-- vim.cmd("packadd kbswitch.nvim")
+-- require("kbswitch").setup()
+
+add({ "https://github.com/riodelphino/macime.nvim" })
+require("macime").setup({
+	vim = {
+		ttimeoutlen = 0, -- Reduce delay after InsertLeave and InsertEnter
+	},
+	save = {
+		enabled = true,
+		scope = "session", -- Save previous IME per nvim pid
+	},
+	socket = {
+		enabled = true, -- Enable `macimed` launchd service for blazing faster switching
+	},
+	-- exclude = {
+	-- 	filetype = { "TelescopePrompt", "snacks_picker_input", "neo-tree-popup", "neo-tree-filter" }, -- Exclude specific filetypes
+	-- },
+})
