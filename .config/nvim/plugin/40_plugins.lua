fb3633598c85b6465465f76dcde660957f61085f
nurlan60 <nurlan.dairbekov@gmail.com>
2026-05-06 05:38:07 +0500

    vim

--- .config/nvim/plugin/40_plugins.lua
+++ .config/nvim/plugin/40_plugins.lua
@@ -121,13 +121,20 @@ later(function()
   -- - `:h conform-options`
   -- - `:h conform-formatters`
   require('conform').setup({
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
     default_format_opts = {
       -- Allow formatting from LSP server if no dedicated formatter is available
       lsp_format = 'fallback',
     },
     -- Map of filetype to formatters
     -- Make sure that necessary CLI tool is available
-    -- formatters_by_ft = { lua = { 'stylua' } },
+    formatters_by_ft = { 
+      lua = { 'stylua' },
+      tex = { 'tex-fmt'},
+    },
   })
 end)
 
