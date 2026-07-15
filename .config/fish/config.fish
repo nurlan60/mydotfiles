fcbed2c4d973ef753fb3949838d38fa94d0007eb
nurlan60 <nurlan.dairbekov@gmail.com>
2026-07-15 15:13:20 +0500

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting ""
+set -gx EDITOR nvim
+
+alias ls='eza --color=always --group-directories-first --icons'
+alias ll='eza -la --icons --octal-permissions --group-directories-first'
+alias v="nvim"
+alias :q="exit"
+alias lg="lazygit"
+alias gac="git add . && git commit -m" # + commit message
+alias gl="git pull"
+alias gp="git push"
+
+# Change Yazi's CWD to PWD on subshell exit
+if [ -n "$YAZI_ID" ]
+	trap 'ya emit cd "$PWD"' EXIT
+end
---
+++ .config/fish/functions/y.fish
@@ -0,0 +1,12 @@
+function y
+  if test -n "$YAZI_LEVEL" 
+    exit
+    return 0
+  end
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	command rm -f -- "$tmp"
+end
