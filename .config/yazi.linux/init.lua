e0a605a39a66d769ed1e80f676ffc576fccd58a7
nurlan60 <nurlan.dairbekov@gmail.com>
2026-07-23 04:36:53 +0500

    yazi

--- .config/yazi.linux/init.lua
+++ .config/yazi.linux/init.lua
@@ -1,9 +1,4 @@
 require("sshfs"):setup()
-require("fuse-archive"):setup({
-  smart_enter = true,
-  excluded_extensions = { "docx", "xlsx"}
-})
-
 -- cross-instance yank
 require("session"):setup({
 	sync_yanked = true,
