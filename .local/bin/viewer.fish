b48e0a619645d76194a4e8b79226b48fd7772cb5
nurlan60 <nurlan.dairbekov@gmail.com>
2026-07-25 19:43:34 +0500

    viewer

--- .local/bin/viewer.fish
+++ .local/bin/viewer.fish
@@ -1,30 +1,45 @@
 #!/bin/zsh
 
-if test (count $argv) -eq 0
+if (( $# == 0 )); then
     echo "Error: Please provide at least one file path."
-    echo "Usage: ./viewer.fish <file>"
+    echo "Usage: ./viewer.zsh <file>"
     exit 1
-end
+fi
 
-set file $argv[1]
+file=$1
 
-switch (path extension $file)
-  case .pdf
-      tdf $file
-  case md
-      leaf $file
-  case .djvu
-      djvutxt $file | bat
-  case .doc
-      antiword $file | bat
-  case .docx
-      doxx $file
-  case .xlsx .xls .xlsm .xlsb .ods
-      xleak -i $file
-  case .json
-      jq -C . $file | bat
-  case .tar .tgz .txz .zip .7z .gz .xz .lzma .lz4 .sz .zst .rar
-      ouch list $file --tree | bat
-  case '*'
-      bat $file
-end
+case $file:e in
+      pdf)
+        tdf $file
+       ;;
+      md)
+        leaf $file
+       ;;
+      djvu)
+        djvutxt $file | bat
+       ;;
+      doc)
+        antiword $file | bat
+        ;;
+      docx)
+        doxx $file
+        ;;
+      xlsx|xls|xlsm|xlsb|ods)
+        xleak -i $file
+        ;;
+      json)
+        jq -C . $file | bat
+        ;;
+      tar|tgz|txz|zip|7z|gz|xz|lzma|lz4|sz|zst|rar)
+        ouch list $file --tree | bat
+        ;;
+      png|jpg|jpeg)
+        viu -w 40 $file 
+        read -k 1 -s "?Press any key to continue..."
+
+        ;;
+        *)
+          bat $file
+          ;;
+esac
+
